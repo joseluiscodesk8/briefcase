b2708b3f8781b4b0f3270747d06e22f8b7e64b7a
jose luis <jlgomezangaritagmail.com>
2022-01-05 21:22:41 -0500

    skate15

--- src/Dom/index.js
+++ src/Dom/index.js
@@ -1,3 +1,20 @@
-const Element = document.getElementById('eyes');
-Element.innerHTML = "skateordie";
+// const Element = document.getElementById('eyes');
+// Element.innerHTML = "skateordie";
 
+
+function myMove () {
+    let id = null;
+    const elem = document.getElementById("nose");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame () {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + "px";
+        }
+    }
+}
