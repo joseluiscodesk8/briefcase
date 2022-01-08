4ea07e8c024bd054580e046cfc0000221de481b6
jose luis <jlgomezangaritagmail.com>
2022-01-08 15:18:07 -0500

    skate19

--- src/Dom/index.js
+++ src/Dom/index.js
@@ -1,6 +1,13 @@
-function meri() {
-    document.getElementById("nose").style.transform = "translate(0, 100%)";
+function caja1() {
+    // document.getElementById("nose").style.transform = "translate(0, 100%)";
     document.getElementById("uno").style.boxShadow = "inset 0 0 5px #000"
     document.getElementById("tres").style.transform = "translate(0, -20rem)"
     document.getElementById("tres").style.transition = "3s"
+    document.getElementById("nose").style.animation = "meri 5s 1"
+}
+
+function caja2() {
+    document.getElementById("eyes").style.transform = "translate(1%)";
+    document.getElementById("eyes").style.transition = "3s";
+    document.getElementById("ojos").style.opacity = "1";
 }
