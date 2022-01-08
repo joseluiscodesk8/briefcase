d047b0faf07f90e81d3f409941f425519d186ed2
jose luis <jlgomezangaritagmail.com>
2022-01-07 22:13:52 -0500

    skate18

---
+++ src/Dom/index.js
@@ -0,0 +1,6 @@
+function meri() {
+    document.getElementById("nose").style.transform = "translate(0, 100%)";
+    document.getElementById("uno").style.boxShadow = "inset 0 0 5px #000"
+    document.getElementById("tres").style.transform = "translate(0, -20rem)"
+    document.getElementById("tres").style.transition = "3s"
+}
