3ba92dc68d92ae182e2db40ccbdd4bff5594e0a8
jose luis <jlgomezangaritagmail.com>
2022-01-08 16:12:06 -0500

    skate20

--- src/Dom/index.js
+++ src/Dom/index.js
@@ -3,11 +3,12 @@ function caja1() {
     document.getElementById("uno").style.boxShadow = "inset 0 0 5px #000"
     document.getElementById("tres").style.transform = "translate(0, -20rem)"
     document.getElementById("tres").style.transition = "3s"
-    document.getElementById("nose").style.animation = "meri 5s 1"
+    document.getElementById("nose").style.animation = "photo 10s 1"
 }
 
 function caja2() {
     document.getElementById("eyes").style.transform = "translate(1%)";
     document.getElementById("eyes").style.transition = "3s";
     document.getElementById("ojos").style.opacity = "1";
+    document.getElementById("cuatro").style.boxShadow = "inset 0 0 5px #000"
 }
